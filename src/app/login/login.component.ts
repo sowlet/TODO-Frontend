4deb788493a3698ef40a45c6c2b4ee07ccf0edb0
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-04-15 21:59:14 -0400

    functional login -> needs server routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from '../app.component';
 
 @Component({
+  imports:[CommonModule, FormsModule, AppComponent],
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private router: Router, private http: HttpClient) {}
+
+  navigateToAbout() {
+    this.router.navigate(['/about']);
+  }
+
+  navigateToSignUp() {
+    this.router.navigate(['/sign-in']);
+  }
+
+  onLogin() {
+    const loginData = { username: this.username, password: this.password };
+    // console.log('Login data:', loginData);
+    this.http.get('/server/login', { params: loginData }).subscribe(
+      (response) => {
+        console.log('Login successful:', response);
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        console.error('Login failed:', error);
+      }
+    );
+  }
 }
