c3fc65c7a959ae0203d4b963310bb93ca25f092b
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-04-21 16:53:20 -0400

    created profile navigation buttons

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HotbarFComponent } from '../components/hotbar-f/hotbar-f.component';
 import { AuthService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -11,9 +12,21 @@ import { AuthService } from '../account.service';
 export class ProfileComponent {
   username: string = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.username = this.authService.getUsername();
+  }
+
+  navigateToProfile() {
+    this.router.navigate(['/profile']);
+  }
+
+  navigateToProfileCT() {
+    this.router.navigate(['/profile-ct']);
+  }
+
+  navigateToAbout() {
+    this.router.navigate(['/about']);
   }
 }
