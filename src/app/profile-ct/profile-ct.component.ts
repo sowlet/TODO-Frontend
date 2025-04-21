70b9481d6c7727519c09b160bfecb8412313fed4
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-04-21 16:56:10 -0400

    copied boiler plate from profile

--- src/app/profile-ct/profile-ct.component.ts
+++ src/app/profile-ct/profile-ct.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { HotbarFComponent } from '../components/hotbar-f/hotbar-f.component';
+import { AuthService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-ct',
-  imports: [],
+  imports: [HotbarFComponent],
   templateUrl: './profile-ct.component.html',
   styleUrl: './profile-ct.component.css'
 })
 export class ProfileCtComponent {
+  username: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.username = this.authService.getUsername();
+  }
+
+  navigateToProfile() {
+    this.router.navigate(['/profile']);
+  }
+
+  navigateToProfileCT() {
+    this.router.navigate(['/profile-ct']);
+  }
+
+  navigateToAbout() {
+    this.router.navigate(['/about']);
+  }
 }
