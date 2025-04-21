b16cde571699bdbde2db754e314abc4b117a880c
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-04-21 15:59:58 -0400

    generated hotbar-f and account service

---
+++ src/app/components/hotbar-f/hotbar-f.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotbar-f',
+  imports: [],
+  templateUrl: './hotbar-f.component.html',
+  styleUrl: './hotbar-f.component.css'
+})
+export class HotbarFComponent {
+
+}
