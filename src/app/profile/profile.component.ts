81a9e3228196521133681ae1f53636dee815c3dc
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-04-21 16:38:47 -0400

    initial profile generation

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { HotbarFComponent } from '../components/hotbar-f/hotbar-f.component';
+import { AuthService } from '../account.service';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [HotbarFComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  username: string = '';
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.username = this.authService.getUsername();
+  }
 }
