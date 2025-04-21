4751731d33ba0a5244611beb451ec8b44ab70362
bennalex2014 <alexander.j.bennett3@gmail.com>
2025-04-21 02:56:39 -0400

    functional sign-up page
    cleaned up formatting for both login and sign up

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from '../app.component';
+import { AuthService } from '../account.service'; // Import the AuthService
 
 @Component({
+  imports:[CommonModule, FormsModule],
   selector: 'app-sign-in',
-  imports: [],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  username = '';
+  email = '';
+  password = '';
 
+  constructor(private router: Router, private http: HttpClient, private authService: AuthService) {}
+
+  navigateToAbout() {
+    this.router.navigate(['/about']);
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  onSignUp() {
+    const signUpData = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.setCredentials(this.username, this.password); // Store credentials
+    this.router.navigate(['/home']);
+    
+    // this.http.post('/server/signup', signUpData).subscribe(
+    //   (response) => {
+    //     console.log('Sign up successful:', response);
+    //     this.router.navigate(['/home']);
+    //   },
+    //   (error) => {
+    //     console.error('Sign up failed:', error);
+    //   }
+    // );
+  }
 }
